d3fd77e6c5f38634ac726f931031fe8cac5e07ef
Owanikin <oderindeife@gmail.com>
2022-08-23 20:08:29 +0100

    using solidity coverage

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("./tasks/block-number")
 require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
