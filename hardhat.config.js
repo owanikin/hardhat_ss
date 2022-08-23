8f50529d3606380984e39677c989acacde432858
Owanikin <oderindeife@gmail.com>
2022-08-23 15:15:06 +0100

    hardhat simple_storage

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+require("@nomicfoundation/hardhat-toolbox")
+// require("@nomiclabs/hardhat-waffle")
+require("dotenv").config()
+require("./tasks/block-number")
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+           url: RINKEBY_RPC_URL,
+           accounts: [PRIVATE_KEY],
+           chainId: 4,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        }
+    },
+    solidity: "0.8.9",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY
+    }
+
+}
