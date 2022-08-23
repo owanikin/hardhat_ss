4597d0b786fb0af250b2111f0f15f81f584b8537
Owanikin <oderindeife@gmail.com>
2022-08-23 20:02:41 +0100

    gasreporter & coinmarket

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox")
 // require("@nomiclabs/hardhat-waffle")
 require("dotenv").config()
 require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -9,6 +10,7 @@ require("./tasks/block-number")
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
 const PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -26,6 +28,13 @@ module.exports = {
     solidity: "0.8.9",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketCap: COINMARKETCAP_API_KEY
     }
 
 }
